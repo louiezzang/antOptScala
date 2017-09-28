2f0cf51a7a6c93576844208edbf7362d5f741874
martin-henke <martin.henke@de.ibm.com>
2017-09-28 16:45:32 +0200

    memoize calculated tour values

--- src/antopt/Tour.scala
+++ src/antopt/Tour.scala
@@ -1,19 +1,19 @@
 package antopt
 
 case class Tour(route: List[Edge], nodes: Map[Int, Point]) {
-  def length =
+  val length =
     route match {
       case head :: _ => route.map(segment => nodes(segment.startNodeId).distance(nodes(segment.targetNodeId))).reduceLeft(_ + _)
       case _         => Long.MaxValue
     }
 
-  def visitedNodes =
+  val visitedNodes =
     route match {
       case head :: _ => head.targetNodeId :: route.map(_.startNodeId)
       case _         => List[Int]()
     }
 
-  def possibleNextEdges =
+  val possibleNextEdges =
     route match {
       case head :: _ => (for (i <- (1 to nodes.size) if (!visitedNodes.contains(i))) yield Edge(route.head.targetNodeId, i)).toList
       case _         => (for (i <- (1 to nodes.size) if (i != 1)) yield Edge(1, i)).toList
